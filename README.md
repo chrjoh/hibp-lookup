1285316fec07d0441b4427075451abb4cc383b5c
chrjoh <christerjoha@gmail.com>
2018-10-11 10:06:30 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+hipb-lookup
+=========
+The goal is to create a simple tool to fetch breache information from the [Have I Been Pwned](https://haveibeenpwned.com/) site given a list of emails.
+
+## Dependencies
+
+## Installation
+
+```bash
+$ go get -u github.com/chrjoh/hibp-lookup
+```
+
+## Usage
+```bash
+$ hibp-lookup --help
+
+Command line arguments:
+
+  -i file with email, one on each line
+```
+
+
+## License (MIT)
+
+Copyright (c) 2018 [Christer Johansson](http://blog.lodakai.com/)
+
+> Permission is hereby granted, free of charge, to any person obtaining
+> a copy of this software and associated documentation files (the
+> "Software"), to deal in the Software without restriction, including
+> without limitation the rights to use, copy, modify, merge, publish,
+> distribute, sublicense, and/or sell copies of the Software, and to
+> permit persons to whom the Software is furnished to do so, subject to
+> the following conditions:
+
+> The above copyright notice and this permission notice shall be
+> included in all copies or substantial portions of the Software.
+
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+> EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+> MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+> NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+> LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+> OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+> WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+var (
+	requestPath = "/api/breachedaccount/"
+
+	emailFile = "email-list.txt"
+	// Command line flags
+	inputFunc = flag.String("i", emailFile, "Input file with one email per line")
+)
+
+func main() {
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "\nCommand line arguments:\n\n")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	// Parse the command line flags
+	flag.Parse()
+	handleQueries(*inputFunc)
+}
+
+func handleQueries(filePath string) {
+	file, err := os.Open(filePath)
+	ba := BreachedAccounts{}
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	h := newHIBP()
+	for scanner.Scan() {
+		email := scanner.Text()
+		fmt.Printf("Checking email: %s\n", email)
+		if result, err := h.GetBreaches(email); err == nil {
+			if err != nil {
+				log.Fatal(err)
+			}
+			if len(result.Breaches) > 0 {
+				ba.Accounts = append(ba.Accounts, *result)
+			}
+		}
+		time.Sleep(2 * time.Second)
+	}
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+	enc.Encode(ba)
+}
+
+type HIBP struct {
+	httpClient *http.Client
+	baseURL    *url.URL
+}
+
+func newHIBP() *HIBP {
+	return &HIBP{
+		httpClient: &http.Client{Timeout: 10 * time.Second},
+		baseURL:    &url.URL{Scheme: "https", Host: "haveibeenpwned.com"},
+	}
+}
+
+type Breaches struct {
+	Breaches []*string `json:"breaches"`
+	Email    string    `json:"email"`
+}
+
+type BreachedAccounts struct {
+	Accounts []Breaches `json:"accounts"`
+}
+
+func (h *HIBP) GetBreaches(email string) (*Breaches, error) {
+	resp, err := h.get(fmt.Sprintf("%s%s", requestPath, url.PathEscape(email)))
+
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	s := []*string{}
+	json.NewDecoder(resp.Body).Decode(&s)
+
+	return &Breaches{Breaches: s, Email: email}, nil
+}
+
+func (h *HIBP) get(path string) (*http.Response, error) {
+	ref, err := url.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return h.httpClient.Get(h.baseURL.ResolveReference(ref).String())
+}
